36a1945f6265ba237f31897235ec72f99b0813b7
LTSteve <Steth1000@gmail.com>
2023-10-14 16:20:01 -0400

    impact

---
+++ card_modifiers/enchanter/impact.gd
@@ -0,0 +1,11 @@
+extends CardModifier
+
+func _apply(weapon,_unit):
+	UniqueMetaId.store([weapon, self, "_create_hit_override"], weapon.create_hit.add_override(_create_hit_override))
+
+func _un_apply(weapon,_unit):
+	weapon.create_hit.remove_override(UniqueMetaId.retrieve([weapon, self, "_create_hit_override"]))
+
+func _create_hit_override(_base_value:Weapon.Hit, current_value:Weapon.Hit, _weapon, _target:Unit):
+	current_value.pushback += params.pushback_per_rank * current_rank
+	return current_value
