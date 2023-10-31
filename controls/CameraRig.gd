bd2c9b75692e69368b9b65bd25c5aad8040166bd
LTSteve <Steth1000@gmail.com>
2023-10-31 13:36:33 -0400

    fixed some wonk

--- controls/CameraRig.gd
+++ controls/CameraRig.gd
@@ -5,7 +5,7 @@ extends Node3D
 @export var camera_distance_min: float = 2
 @export var camera_distance_max: float = 30
 
-@export var camera_scroll_speed: float = 10
+@export var camera_scroll_speed: float = 80
 @export var camera_rotation_speed: float = 90
 @export var camera_angle_speed: float = 20
 @export var camera_distance_speed: float = 3
@@ -39,11 +39,11 @@ var distance_value: float:
 	set(value):
 		distance_value = clampf(value,camera_distance_min,camera_distance_max)
 
-var left_right_input: int
-var forward_back_input: int
-var rotate_input: int
-var angle_input: int
-var distance_input: int
+var left_right_input: float
+var forward_back_input: float
+var rotate_input: float
+var angle_input: float
+var distance_input: float
 
 func _ready():
 	angle_value = camera_angle_min + (camera_angle_max - camera_angle_min) / 2.0
@@ -54,10 +54,10 @@ func _process(delta):
 		forward_back_input = -1
 		rotate_input = 1
 	else:
-		var drag_v2:Vector2 = TouchAndMouseInput.I.get_current_drag_v2() * drag_speed
+		var drag_v2:Vector2 = TouchAndMouseInput.I.get_current_drag_v2()
 		#input
-		left_right_input = _combine_actions("camera_right", "camera_left") - drag_v2.x
-		forward_back_input = _combine_actions("camera_back", "camera_forward") - drag_v2.y
+		left_right_input = _combine_actions("camera_right", "camera_left") * camera_scroll_speed - drag_v2.x * drag_speed
+		forward_back_input = _combine_actions("camera_back", "camera_forward") * camera_scroll_speed - drag_v2.y * drag_speed
 		rotate_input = _combine_actions("camera_rotate_right", "camera_rotate_left")
 		angle_input = _combine_actions("camera_angle_up", "camera_angle_down")
 		distance_input = _combine_actions("camera_zoom", "camera_dezoom")
@@ -65,7 +65,7 @@ func _process(delta):
 	#movement
 	var desired_movement = left_right_input * Vector3.LEFT + forward_back_input * Vector3.FORWARD
 	
-	velocity_component.accelerate_to_velocity(Math.v3_to_v2(desired_movement) * camera_scroll_speed, delta)
+	velocity_component.accelerate_to_velocity(Math.v3_to_v2(desired_movement), delta)
 	translate(camera_rotation.quaternion * Math.v2_to_v3(velocity_component.apply_delta(delta), global_position.y))
 	global_position.y = Ground.sample_height(camera.global_position.x, camera.global_position.z) + 0.1
 	
@@ -81,5 +81,5 @@ func _process(delta):
 	distance_value += distance_input * camera_distance_speed * delta
 	camera_distance.position = Vector3(0,0,-distance_value)
 
-func _combine_actions(positive_input:String, negative_input:String) -> int:
-	return (Input.get_action_strength(positive_input) - Input.get_action_strength(negative_input)) as int
+func _combine_actions(positive_input:String, negative_input:String) -> float:
+	return (Input.get_action_strength(positive_input) - Input.get_action_strength(negative_input)) as float
