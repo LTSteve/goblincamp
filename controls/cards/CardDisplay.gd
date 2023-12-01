18a325f8bd4d65f5c2165e80cfacdab32ad632df
LTSteve <Steth1000@gmail.com>
2023-11-30 19:29:50 -0500

    fix errors thrown around card ui viewport texture

--- controls/cards/CardDisplay.gd
+++ controls/cards/CardDisplay.gd
@@ -10,6 +10,10 @@ class_name CardDisplay
 
 @onready var material_override: MaterialOverrideComponent = $"MaterialOverrideComponent"
 
+#have to do this to avoid errors
+@onready var ui_material_override: MaterialOverrideComponent = $"MeshInstance3D2/MaterialOverrideComponent"
+@onready var ui_viewport: SubViewport = $"SubViewport"
+
 var show_next_rank: bool = false
 
 func _ready():
@@ -21,3 +25,10 @@ func _ready():
 	var current_rank = current_modifier.current_rank
 	var text = card_resource.descriptions[min(card_resource.descriptions.size() - 1, current_rank)]
 	card_description.text = ExpressionsParser.parse(text, { "current_rank": current_rank + (1 if show_next_rank else 0)}, current_modifier.params)
+	
+	call_deferred("_assign_ui_texture")
+
+func _assign_ui_texture():
+	var ui_material = ui_material_override.get_material()
+	
+	ui_material.albedo_texture = ui_viewport.get_texture()
