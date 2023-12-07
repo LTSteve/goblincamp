08a902ae25ee758e9f39acb716bac477b0030432
LTSteve <Steth1000@gmail.com>
2023-12-07 07:43:47 -0500

    cards follow mouse a little bit

--- controls/cards/CardDisplay.gd
+++ controls/cards/CardDisplay.gd
@@ -8,23 +8,31 @@ class_name CardDisplay
 @export var card_title: Label
 @export var card_description: Label
 
-@onready var material_override: MaterialOverrideComponent = $"MaterialOverrideComponent"
+@onready var material_override: MaterialOverrideComponent = $"card_facer/MaterialOverrideComponent"
 
 #have to do this to avoid errors
-@onready var ui_material_override: MaterialOverrideComponent = $"MeshInstance3D2/MaterialOverrideComponent"
-@onready var ui_viewport: SubViewport = $"SubViewport"
+@onready var ui_material_override: MaterialOverrideComponent = $"card_facer/MeshInstance3D2/MaterialOverrideComponent"
+@onready var ui_viewport: SubViewport = $"card_facer/SubViewport"
 
-@onready var texture_rect_3d: TextureRect3D = $"SubViewport/Theme/VBoxContainer/MarginContainer2/3DTextureRect"
+@onready var texture_rect_3d: TextureRect3D = $"card_facer/SubViewport/Theme/VBoxContainer/MarginContainer2/3DTextureRect"
+
+@onready var card_model: Node3D = $"card_facer"
 
 var hidden := true
 var no_flip := false
 
 var show_next_rank: bool = false
 
-var _tween: Tween
+var _flip_tween: Tweenifier
+var _look_at_tween: Tweenifier
+
+var _main_cam: Camera3D
 
 func _ready():
+	_flip_tween = Tweenifier.new(self, "rotation_degrees", 0.2)
+	_look_at_tween = Tweenifier.new(card_model, "rotation", 0.1)
 	if card_resource: initialize(hidden)
+	_main_cam = CameraRig.I.camera
 
 func initialize(flipped:=true):
 	if ! card_resource: return
@@ -48,21 +56,39 @@ func initialize(flipped:=true):
 
 func flip():
 	if no_flip: return
-	_set_up_tween()
 	if hidden:
 		hidden = false
-		_tween.tween_property(self, "rotation_degrees", Vector3(0,0,0), 0.2)
+		_flip_tween.tween(Vector3(0,0,0))
 	else:
 		hidden = true
-		_tween.tween_property(self, "rotation_degrees", Vector3(0,180,0), 0.2)
-
-func _set_up_tween():
-	if _tween:
-		_tween.stop()
-		_tween = null
-	_tween = create_tween()
-	_tween.set_ease(Tween.EASE_IN_OUT)
+		_flip_tween.tween(Vector3(0,180,0))
 
 func _assign_ui_texture():
 	var ui_material = ui_material_override.get_material()
 	ui_material.albedo_texture = ui_viewport.get_texture()
+
+func _mouse_hover_exited():
+	_tween_to_direction(card_model.global_transform.basis.x)
+
+func _mouse_input_event(_camera, event, _position, normal, _shape_idx):
+	if event is InputEventMouseMotion:
+		_tween_to_direction(normal)
+
+func _tween_to_direction(dir: Vector3):
+	var forward = card_model.global_transform.basis.x
+	var rot_q: Quaternion
+	
+	if forward == dir:
+		rot_q = Quaternion.IDENTITY
+	else:
+		var a = forward.cross(dir)
+		rot_q = Quaternion(a.x,a.y,a.z, 1 + forward.dot(dir))
+		rot_q = rot_q.normalized()
+	
+	var rot_e := rot_q.get_euler()
+	
+	print("look at")
+	print(forward)
+	print(rot_e)
+	
+	_look_at_tween.tween(rot_e)
---
+++ util/Tweenifier.gd
@@ -0,0 +1,31 @@
+class_name Tweenifier
+
+var _me: Node
+var _property: String
+var _slide_time: float
+var _trans_type: Tween.TransitionType
+var _ease_type: Tween.EaseType
+
+var _tween: Tween
+
+func _init(me:Node, property: String, slide_time: float
+, ease_type: Tween.EaseType = Tween.EaseType.EASE_IN_OUT
+, trans_type:Tween.TransitionType = Tween.TransitionType.TRANS_LINEAR):
+	_me = me
+	_property = property
+	_slide_time = slide_time
+	_ease_type = ease_type
+	_trans_type = trans_type
+
+func tween(value):
+	_tween = _reset_tween()
+	_tween.tween_property(_me, _property, value, _slide_time)
+
+func _reset_tween() -> Tween:
+	if _tween:
+		_tween.stop()
+		_tween = null
+	_tween = _me.create_tween()
+	_tween.set_ease(_ease_type)
+	_tween.set_trans(_trans_type)
+	return _tween
